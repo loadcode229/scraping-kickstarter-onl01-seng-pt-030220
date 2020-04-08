e68f4fdfbb455dcc26f5ebae3eb0f090d6ef4ed3
Adam Priest <sepone229@gmail.com>
2020-04-08 10:04:41 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,6 +1,7 @@
 # require libraries/modules here
-requie 'nokogirl'
+requie 'nokogiri'
 
 def create_project_hash
-  # write your code here
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri
 end
